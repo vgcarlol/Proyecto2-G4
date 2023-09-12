6afe96c5aaad69befa8a24dba9952ac2f6e74775
cor22982 <cor22982@uvg.edu.gt>
2023-09-12 15:28:36 -0600

    Agregar programa inicial

---
+++ Prog2-G4.cpp
@@ -0,0 +1,23 @@
+/*
+*----------------------------------------
+* PROYECTO 2 GRUPO 4
+* ---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3086 - Programacion de Microprocesadores
+* Temario: 4 - Estudio de tiempos y movimientos de ventanillas de LA VAS
+* ---------------------------------------
+*
+* Mathew Cordero 22982
+* Josue Say 22801
+* Abby Donis 22440
+* Carlos Valladares 221164
+*----------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main (){
+
+    
+}

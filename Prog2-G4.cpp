670abb5bd8b2c7c00a6b7672f4bbdd98de285771
cor22982 <cor22982@uvg.edu.gt>
2023-09-13 15:58:26 -0600

    Agregar manejo de variables.

--- Prog2-G4.cpp
+++ Prog2-G4.cpp
@@ -25,12 +25,13 @@ int carros_compass;
 int carros_efectivo;
 int *tiempos_efectivo, *tiempos_compass;
 int tiempoTotal;
+int Ttiempo;
 
 //void atencion_efectivo();
 void atencion(int carros,int *array,string tipo,int *tiempos,int tipo_tiempo);
-
+void sumaTiempos();
+void llenartiempos();
 int main (){
-
     //Pedimos los datos que necesitamos. 
     cout << "Ingrese el numero de carros que usan compass: ";
     cin >> carros_compass;
@@ -42,51 +43,55 @@ int main (){
     efectivo = new int[carros_efectivo];
     tiempos_efectivo = new int[3];
     tiempos_compass = new int[3];
-    
-    // llenando de los punteros. 
-    for (int i =0;i<carros_compass;i++){
-        compass[i] = i;
-    }
 
-    for (int i =0;i<carros_efectivo;i++){
-        efectivo[i] = i;
-    }
-
+    llenartiempos();
+    atencion(carros_compass, compass, "compass", tiempos_compass, tiempo_compass);
+    atencion(carros_efectivo, efectivo, "efectivo", tiempos_efectivo, tiempo_efectivo);
+    sumaTiempos();
     
-    //Realizaremos 2 tareas que llevan a cabo esto. 
-    //
-    atencion(carros_compass,compass,"compass",tiempos_compass,tiempo_compass);
+    
 }
 
+/// @brief funcion que atiende a los carros. 
+/// @param carros 
+/// @param array 
+/// @param tipo 
+/// @param tiempos 
+/// @param tipo_tiempo 
 void atencion(int carros,int *array,string tipo,int *tiempos,int tipo_tiempo){
     int n = carros-1;
+    tiempos[0]=0;
+    tiempos[1]=0;
+    tiempos[2]=0;
+
     for (int i = 0; i <= n; i+=3)
     {
         #pragma omp sections
         {
             #pragma omp section
-            {
-                tiempoTotal += tipo_tiempo;
+            { 
+                #pragma omp atomic
                 tiempos[0]+=tipo_tiempo;
-                cout << "El carro: " << array[i]<< " es atendido en el kisko 1 de tipo "<<tipo<< endl;
-                
+                cout << "\nEl carro: " << array[i]<< " es atendido en el kisko 1 de tipo "<<tipo<<endl; 
             }
             #pragma omp section
             {
                 if(i < n)
                 {
-                    tiempoTotal += tipo_tiempo;
+                    #pragma omp atomic
                     tiempos[1]+=tipo_tiempo;
-                    cout << "El carro: " << array[i+1]<< " es atendido en el kisko 2 de tipo "<<tipo<< endl;
+                    cout << "\nEl carro: " << array[i+1]<< " es atendido en el kisko 2 de tipo "<<tipo<<endl;
+                  
                 }
             }
             #pragma omp section
             {
                 if(i < n-1)
-                {
-                    tiempoTotal += tipo_tiempo;
+                {   
+                    #pragma omp atomic
                     tiempos[2]+=tipo_tiempo;
-                    cout << "El carro: " << array[i+2]<< " es atendido en el kisko 3 de tipo "<<tipo<< endl;
+                    cout << "\nEl carro: " << array[i+2]<< " es atendido en el kisko 3 de tipo "<<tipo<<endl;
+                    
                 }
             }
         }
@@ -94,4 +99,27 @@ void atencion(int carros,int *array,string tipo,int *tiempos,int tipo_tiempo){
         
     }
     
+}
+
+/// @brief funcion que suma todos los tiempos e imprime el tiempo total 
+void sumaTiempos(){
+    int suma = 0;
+    #pragma omp parallel for reduction(+:suma)
+    for (int i = 0; i < 3; i++) {
+        suma += tiempos_efectivo [i]+tiempos_compass[i];
+    } 
+    cout <<"\nEl tiempo en total para atender a "<<carros_compass<< " carros compass y "<<carros_efectivo<<" carros efectivo es de: "<<suma<< endl;
+}
+
+void llenartiempos(){
+    // llenando de los punteros.
+    #pragma omp parallel for 
+    for (int i =0;i<carros_compass;i++){
+        compass[i] = i;
+    }
+
+    #pragma omp parallel for
+    for (int i =0;i<carros_efectivo;i++){
+        efectivo[i] = i;
+    }
 }
